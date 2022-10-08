1e89fd9f4a6c4c41a387d636415ddf42234a88ca
ScaleMind <92308124+KhoiDOO@users.noreply.github.com>
2022-10-08 22:03:11 +0700

    Update Orb Invariant Checking

--- Data Preprocessing/orb.py
+++ Data Preprocessing/orb.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     print("SIFT Json Test file: {}".format(opt.target_sift_json_test_file))
 
     # print(test(opt.ori_train_files[:100])) # (201.29, 2257)
-    print(test(opt.ori_train_files + opt.ori_test_files)) # (291.4488636363636, 2582)
+    # print(test(opt.ori_train_files + opt.ori_test_files)) # (671.4488636363636, 2582)
 
     # sift_extract(data_file_path=opt.ori_train_files, target_path=opt.target_sift_json_train_file)
     # sift_extract(data_file_path=opt.ori_test_files, target_path=opt.target_sift_json_test_file)
@@ -112,9 +112,72 @@ if __name__ == '__main__':
     #             target_path=opt.target_sift_json_test_file,
     #             nfeatures = 1000)
 
-    orb_extract(data_file_path=opt.ori_train_files, 
-                target_path=opt.target_sift_json_train_file,
-                nfeatures = 10000)
-    orb_extract(data_file_path=opt.ori_test_files, 
-                target_path=opt.target_sift_json_test_file,
-                nfeatures = 10000)
+    # orb_extract(data_file_path=opt.ori_train_files, 
+    #             target_path=opt.target_sift_json_train_file,
+    #             nfeatures = 10000)
+    # orb_extract(data_file_path=opt.ori_test_files, 
+    #             target_path=opt.target_sift_json_test_file,
+    #             nfeatures = 10000)
+    
+    img = cv2.imread(opt.ori_train_files[0])
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    orb = cv2.ORB_create(nfeatures = 1000)
+    kp, des = orb.detectAndCompute(gray,None)
+
+    scale_percent = 60 
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    gray_resized = cv2.cvtColor(resized,cv2.COLOR_BGR2GRAY)
+    gray_resized_kp, gray_resized_des = orb.detectAndCompute(gray_resized,None)
+    resized_img_keys = cv2.drawKeypoints(gray_resized,gray_resized_kp,resized,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imwrite('Data Preprocessing/orb_invariant_check/resized_orb_keypoints.jpg',resized_img_keys)
+
+    bf = cv2.BFMatcher()
+    # matches = bf.knnMatch(des,gray_resized_des,k=2)
+
+    # good = []
+    # for m,n in matches:
+    #     if m.distance < 0.75*n.distance:
+    #         good.append([m])
+    # scaled_match = cv2.drawMatchesKnn(gray,kp,gray_resized,gray_resized_kp,good,None,flags=cv2.DrawMatchesFlags_DEFAULT)
+    # cv2.imwrite('Data Preprocessing/orb_invariant_check/scaledmatch_orb.jpg',scaled_match)
+
+    # FLANN_INDEX_KDTREE = 1
+    # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+
+    # search_params = dict(checks=50)
+    
+    # flann = cv2.FlannBasedMatcher(index_params,search_params)
+
+    # matches = flann.knnMatch(des,gray_resized_des,k=2)
+
+    # matchesMask = [[0,0] for i in range(len(matches))]
+
+    # for i,(m,n) in enumerate(matches):
+    #     if m.distance < 0.7*n.distance:
+    #         matchesMask[i]=[1,0]
+    # draw_params = dict(matchColor = (0,255,0),
+    #                singlePointColor = (255,0,0),
+    #                matchesMask = matchesMask,
+    #                flags = cv2.DrawMatchesFlags_DEFAULT)
+    # scale_match_flann = cv2.drawMatchesKnn(gray,kp,gray_resized,gray_resized_kp,matches,None,**draw_params)
+    # cv2.imwrite('Data Preprocessing/orb_invariant_check/scaledmatch_orb_flann.jpg',scale_match_flann)
+
+    (h, w) = img.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+    
+    M = cv2.getRotationMatrix2D((cX, cY), -160, 1.0)
+    rotated = cv2.warpAffine(img, M, (w, h))
+    rotated_gray = cv2.cvtColor(rotated,cv2.COLOR_BGR2GRAY)
+    gray_rotate_kp, gray_rotate_des = orb.detectAndCompute(rotated_gray,None)
+    matches = bf.knnMatch(des,gray_rotate_des,k=2)
+
+    good = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append([m])
+    rotate_match = cv2.drawMatchesKnn(gray,kp,rotated_gray,gray_rotate_kp,good,None,flags=cv2.DrawMatchesFlags_DEFAULT)
+    cv2.imwrite('Data Preprocessing/orb_invariant_check/rotatematch_orb.jpg',rotate_match)
