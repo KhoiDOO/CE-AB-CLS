522bff612482154138a14229f9fa0b4c125c9330
ScaleMind <95898047+ScaleMind@users.noreply.github.com>
2022-10-29 15:41:29 +0700

    Create KM Data set

---
+++ Data Preprocessing/bag_of_visual_word/k_mean.py
@@ -0,0 +1,71 @@
+import os 
+import glob
+import json 
+import numpy as np
+import pandas as pd
+
+# print(os.getcwd())
+os.chdir("..")
+os.chdir("..")
+# print(os.getcwd())
+
+main_data_dir = os.getcwd() + "\\Data set"
+sift_data = main_data_dir + "\\sift"
+akaze_data = main_data_dir + "\\akaze"
+kaze_data = main_data_dir + "\\kaze"
+orb_data = main_data_dir + "\\orb"
+
+# print(sift_data)
+# print(akaze_data)
+# print(kaze_data)
+# print(orb_data)
+
+sift_data_train = [i for i in glob.glob(sift_data + '\\*') if 'train' in i]
+sift_data_test = [i for i in glob.glob(sift_data + '\\*') if 'test' in i]
+
+# print(sift_data_train)
+# print(sift_data_test)
+
+def load_json(path):
+    f = open(path)
+    return  json.load(f)
+
+sift_train_json = load_json(sift_data_train[0])
+sift_test_json = load_json(sift_data_test[0])
+
+
+def create_kmean_dataset(json_data, path = None):
+    normal_dict = json_data["Group 1 - Normal"]
+    abnormal_dict = json_data["Group 2 - Abnormal"]
+
+    feature_col = [f"feature_{i+1}" for i in range(128)]
+
+    concat_lst = []
+
+    for img_dict in normal_dict:
+        for keys in img_dict:
+            img_vector_lst = img_dict[keys]
+            img_vector_normal = np.array(img_vector_lst)
+            concat_lst.append(img_vector_normal)
+    
+    for img_dict in abnormal_dict:
+        for keys in img_dict:
+            img_vector_lst = img_dict[keys]
+            img_vector_abnormal = np.array(img_vector_lst)
+            concat_lst.append(img_vector_abnormal)
+
+    # concat_data = np.concatenate((img_vector_normal, img_vector_abnormal), axis=0)
+    
+    base = concat_lst[0]
+    for x in range(1, len(concat_lst)):
+        concat_data = np.concatenate((base, concat_lst[x]), axis=0)
+        base = concat_data
+
+    if path:
+        df = pd.DataFrame(concat_data, columns=feature_col)
+        df.to_csv(path)
+
+
+
+save_path = main_data_dir + "\\kmean_dataset\\" + sift_data_train[0].split("\\")[-1].replace("json", "csv")
+create_kmean_dataset(sift_train_json, save_path)
