0a11196f197387027dd762ff6e66337d9ff0307b
Do Hoang Khoi <92308124+KhoiDOO@users.noreply.github.com>
2022-11-04 14:19:33 +0700

    Update KM 10 fold Preprocessing

--- Data Preprocessing/sift.py
+++ Data Preprocessing/sift.py
@@ -104,84 +104,84 @@ if __name__ == '__main__':
     #             _nfeatures = 0)
 
 
-    img = cv2.imread(opt.ori_train_files[100])
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    sift = cv2.SIFT_create()
+    # img = cv2.imread(opt.ori_train_files[100])
+    # gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # sift = cv2.SIFT_create()
     
-    kp, des = sift.detectAndCompute(gray,None)
-    img_keys=cv2.drawKeypoints(gray,kp,img,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imwrite('Data Preprocessing/sift_invariant_check/sift_keypoints.jpg',img_keys)
+    # kp, des = sift.detectAndCompute(gray,None)
+    # img_keys=cv2.drawKeypoints(gray,kp,img,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # cv2.imwrite('Data Preprocessing/sift_invariant_check/sift_keypoints.jpg',img_keys)
 
-    scale_percent = 60 
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
-    dim = (width, height)
+    # scale_percent = 60 
+    # width = int(img.shape[1] * scale_percent / 100)
+    # height = int(img.shape[0] * scale_percent / 100)
+    # dim = (width, height)
   
-    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    gray_resized = cv2.cvtColor(resized,cv2.COLOR_BGR2GRAY)
-    gray_resized_kp, gray_resized_des = sift.detectAndCompute(gray_resized,None)
-    resized_img_keys = cv2.drawKeypoints(gray_resized,gray_resized_kp,resized,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imwrite('Data Preprocessing/sift_invariant_check/resized_sift_keypoints.jpg',resized_img_keys)
+    # resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    # gray_resized = cv2.cvtColor(resized,cv2.COLOR_BGR2GRAY)
+    # gray_resized_kp, gray_resized_des = sift.detectAndCompute(gray_resized,None)
+    # resized_img_keys = cv2.drawKeypoints(gray_resized,gray_resized_kp,resized,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # cv2.imwrite('Data Preprocessing/sift_invariant_check/resized_sift_keypoints.jpg',resized_img_keys)
 
-    bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des,gray_resized_des,k=2)
+    # bf = cv2.BFMatcher()
+    # matches = bf.knnMatch(des,gray_resized_des,k=2)
 
-    good = []
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            good.append([m])
-    scaled_match = cv2.drawMatchesKnn(gray,kp,gray_resized,gray_resized_kp,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    cv2.imwrite('Data Preprocessing/sift_invariant_check/scaledmatch_sift.jpg',scaled_match)
+    # good = []
+    # for m,n in matches:
+    #     if m.distance < 0.75*n.distance:
+    #         good.append([m])
+    # scaled_match = cv2.drawMatchesKnn(gray,kp,gray_resized,gray_resized_kp,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    # cv2.imwrite('Data Preprocessing/sift_invariant_check/scaledmatch_sift.jpg',scaled_match)
 
-    FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    # FLANN_INDEX_KDTREE = 1
+    # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 
-    search_params = dict(checks=50)
+    # search_params = dict(checks=50)
     
-    flann = cv2.FlannBasedMatcher(index_params,search_params)
-    matches = flann.knnMatch(des,gray_resized_des,k=2)
+    # flann = cv2.FlannBasedMatcher(index_params,search_params)
+    # matches = flann.knnMatch(des,gray_resized_des,k=2)
 
-    matchesMask = [[0,0] for i in range(len(matches))]
+    # matchesMask = [[0,0] for i in range(len(matches))]
 
-    for i,(m,n) in enumerate(matches):
-        if m.distance < 0.7*n.distance:
-            matchesMask[i]=[1,0]
-    draw_params = dict(matchColor = (0,255,0),
-                   singlePointColor = (255,0,0),
-                   matchesMask = matchesMask,
-                   flags = cv2.DrawMatchesFlags_DEFAULT)
-    scale_match_flann = cv2.drawMatchesKnn(gray,kp,gray_resized,gray_resized_kp,matches,None,**draw_params)
-    cv2.imwrite('Data Preprocessing/sift_invariant_check/scaledmatch_sift_flann.jpg',scale_match_flann)
+    # for i,(m,n) in enumerate(matches):
+    #     if m.distance < 0.7*n.distance:
+    #         matchesMask[i]=[1,0]
+    # draw_params = dict(matchColor = (0,255,0),
+    #                singlePointColor = (255,0,0),
+    #                matchesMask = matchesMask,
+    #                flags = cv2.DrawMatchesFlags_DEFAULT)
+    # scale_match_flann = cv2.drawMatchesKnn(gray,kp,gray_resized,gray_resized_kp,matches,None,**draw_params)
+    # cv2.imwrite('Data Preprocessing/sift_invariant_check/scaledmatch_sift_flann.jpg',scale_match_flann)
 
-    (h, w) = img.shape[:2]
-    (cX, cY) = (w // 2, h // 2)
+    # (h, w) = img.shape[:2]
+    # (cX, cY) = (w // 2, h // 2)
     
-    M = cv2.getRotationMatrix2D((cX, cY), -160, 1.0)
-    rotated = cv2.warpAffine(img, M, (w, h))
-    rotated_gray = cv2.cvtColor(rotated,cv2.COLOR_BGR2GRAY)
-    gray_rotate_kp, gray_rotate_des = sift.detectAndCompute(rotated_gray,None)
-    matches = bf.knnMatch(des,gray_rotate_des,k=2)
+    # M = cv2.getRotationMatrix2D((cX, cY), -160, 1.0)
+    # rotated = cv2.warpAffine(img, M, (w, h))
+    # rotated_gray = cv2.cvtColor(rotated,cv2.COLOR_BGR2GRAY)
+    # gray_rotate_kp, gray_rotate_des = sift.detectAndCompute(rotated_gray,None)
+    # matches = bf.knnMatch(des,gray_rotate_des,k=2)
 
-    good = []
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            good.append([m])
-    rotate_match = cv2.drawMatchesKnn(gray,kp,rotated_gray,gray_rotate_kp,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    cv2.imwrite('Data Preprocessing/sift_invariant_check/rotatematch_sift.jpg',rotate_match)
+    # good = []
+    # for m,n in matches:
+    #     if m.distance < 0.75*n.distance:
+    #         good.append([m])
+    # rotate_match = cv2.drawMatchesKnn(gray,kp,rotated_gray,gray_rotate_kp,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    # cv2.imwrite('Data Preprocessing/sift_invariant_check/rotatematch_sift.jpg',rotate_match)
 
-    matches = flann.knnMatch(des,gray_rotate_des,k=2)
+    # matches = flann.knnMatch(des,gray_rotate_des,k=2)
 
-    matchesMask = [[0,0] for i in range(len(matches))]
+    # matchesMask = [[0,0] for i in range(len(matches))]
 
-    for i,(m,n) in enumerate(matches):
-        if m.distance < 0.7*n.distance:
-            matchesMask[i]=[1,0]
-    draw_params = dict(matchColor = (0,255,0),
-                   singlePointColor = (255,0,0),
-                   matchesMask = matchesMask,
-                   flags = cv2.DrawMatchesFlags_DEFAULT)
-    rotatematch_flann = cv2.drawMatchesKnn(gray,kp,rotated_gray,gray_rotate_kp,matches,None,**draw_params)
-    cv2.imwrite('Data Preprocessing/sift_invariant_check/rotatematch_sift_flann.jpg',rotatematch_flann)
+    # for i,(m,n) in enumerate(matches):
+    #     if m.distance < 0.7*n.distance:
+    #         matchesMask[i]=[1,0]
+    # draw_params = dict(matchColor = (0,255,0),
+    #                singlePointColor = (255,0,0),
+    #                matchesMask = matchesMask,
+    #                flags = cv2.DrawMatchesFlags_DEFAULT)
+    # rotatematch_flann = cv2.drawMatchesKnn(gray,kp,rotated_gray,gray_rotate_kp,matches,None,**draw_params)
+    # cv2.imwrite('Data Preprocessing/sift_invariant_check/rotatematch_sift_flann.jpg',rotatematch_flann)
 
     # gray_cropped_image = gray[50:320, 110:370]
     # cv2.imshow("cropped", cropped_image)
